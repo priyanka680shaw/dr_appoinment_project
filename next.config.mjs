29f2de5df628c8d3d629d13637af150a642f9def
priyanka680shaw <priyankashaw680@gmail.com>
2025-04-15 16:43:03 +0530

    adding netlify files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+    
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true, // good practice for development
+  };
+  
+  export default nextConfig;
+  
